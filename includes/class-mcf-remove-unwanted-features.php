dd63f6bec65e23a11034604130420c87bf46e669
Giacomo Secchi <giacomosecchi@gmail.com>
2023-03-01 11:18:14 +0100

    Add unwanted features class

--- includes/class-mcf-remove-unwanted-features.php
+++ includes/class-mcf-remove-unwanted-features.php
@@ -17,18 +17,46 @@ class MCF_Remove_Unwated_Features {
 	 */
 	public function __construct() {
 
+		add_action( 'init', array( $this, 'stop_heartbeat' ), 1 );
+
+		$this->turn_off_stuff();
+
+	}
+
+	/**
+     * Disable Heartbeat
+     *
+     * @since  1.0.0
+     * @access public
+	 * @link https://it.siteground.com/tutorial/wordpress/limitare-heartbeat/
+     * @return void
+     */
+	function stop_heartbeat() {
+		wp_deregister_script('heartbeat');
+	}
+
+
+
+	/**
+     * Disable features
+     *
+     * @since  1.0.0
+     * @access public
+     * @return void
+     */
+	function turn_off_stuff() {
 		$hook_names = array(
 			'big_image_size_threshold'	// https://make.wordpress.org/core/2019/10/09/introducing-handling-of-big-images-in-wordpress-5-3/
 		);
 
-		foreach ( $hook_names as $hook_name ) {
+
+		foreach ( apply_filters( 'mcf_disable_features', $hook_names ) as $hook_name ) {
 			if ( empty( $hook_name ) ) {
 				return;
 			}
 
+
 			add_filter( $hook_name, '__return_false' );
 		}
-
 	}
-
 }
