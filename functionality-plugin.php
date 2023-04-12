ab0b61de8d396d60052d6a92c31f944dd13fc709
giacomo.secchi <giacomosecchi@gmail.com>
2023-04-12 11:54:05 +0200

    Update version

--- functionality-plugin.php
+++ functionality-plugin.php
@@ -5,7 +5,7 @@
  * Description: This is an awesome custom plugin with functionality that I'd like to keep when switching things.
  * Author: Giacomo Secchi
  * Author URI: https://giacomosecchi.com
- * Version: 0.0.8
+ * Version: 0.0.9
  */
 
 /* Place custom code below this line. */
