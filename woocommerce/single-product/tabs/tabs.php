0afca189a1ba2728eebccdcde561c6a55f9ac2d6
Giacomo Secchi <giacomosecchi@gmail.com>
2023-04-16 19:30:45 +0200

    Add default template for tabs

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -16,7 +16,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
@@ -29,7 +29,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $product_tabs ) ) : ?>
-        <div class="accordion-container">
+
+	// If MCF_WOOCOMMERCE_SINGLE_PRODUCT_ADDITIONAL_INFORMATIONS_LAYOUT == 'accordion'
+	<div class="accordion-container">
             <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
                 <div class="accordion-section <?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
                     <a class="accordion-section-title">
@@ -45,7 +47,30 @@ if ( ! empty( $product_tabs ) ) : ?>
                 </div>
             <?php endforeach; ?>
         </div>
-        <?php do_action( 'woocommerce_product_after_tabs' ); ?>
-   
+
+	// Else
+
+	<div class="woocommerce-tabs wc-tabs-wrapper">
+		<ul class="tabs wc-tabs" role="tablist">
+			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+				<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+					<a href="#tab-<?php echo esc_attr( $key ); ?>">
+						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+				<?php
+				if ( isset( $product_tab['callback'] ) ) {
+					call_user_func( $product_tab['callback'], $key, $product_tab );
+				}
+				?>
+			</div>
+		<?php endforeach; ?>
+
+		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
+	</div>
 
 <?php endif; ?>
