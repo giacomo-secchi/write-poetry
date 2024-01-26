052844a61c438b2ca9f9fced018cbf5932b78e20
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-01-26 14:35:52 +0100

    Update import paths for icons

--- src/blocks/animation/variations.js
+++ src/blocks/animation/variations.js
@@ -6,7 +6,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { ballIcon, animationIcon } from './icons';
+import { ballIcon, animationIcon } from '../../packages/icons';
 
 /** @typedef {import('@wordpress/blocks').WPBlockVariation} WPBlockVariation */
 
