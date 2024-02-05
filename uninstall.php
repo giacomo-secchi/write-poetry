1643ec6670510377b8e23eacd1d6fe91d7040aaf
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-02-05 22:04:13 +0100

    Refactor uninstall.php file

--- uninstall.php
+++ uninstall.php
@@ -1,22 +1,21 @@
 <?php
 /**
  * Functionality that is executed when WritePoetry is uninstalled via built-in WordPress commands.
- *
  */
 
 
 // if uninstall.php is not called by WordPress, die
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
+	die;
 }
 
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 }
 
-use \WritePoetry\Api\PluginConfig;
+use WritePoetry\Api\PluginConfig;
 
-$config = PluginConfig::getInstance();
+$config      = PluginConfig::getInstance();
 $all_options = wp_load_alloptions();
 
 // Delete all plugin options in wp_otions table
@@ -31,4 +30,3 @@ foreach ( $all_options as $option_name => $option_value ) {
 
 // Clear any cached data that has been removed.
 wp_cache_flush();
-
